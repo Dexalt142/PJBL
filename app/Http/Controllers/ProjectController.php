9b7ba57568573b479af4e7eab7d6edad2fcf1396
Dexalt142 <faturjackson14@gmail.com>
2020-05-23 21:14:35 +0700

    Show project details

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Project;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Collection;
 
 class ProjectController extends Controller {
+
+    public function __construct() {
+        $this->middleware('guru', ['only' => ['buatProject', 'showProjectPage', 'viewProject']]);
+    }
 
     public function buatProject(Request $request) {
         $validated = $request->validate([
@@ -17,6 +22,22 @@ class ProjectController extends Controller {
         if(Project::create($validated)) {
             return redirect()->to($request->r);
         }
+    }
+
+    public function showProjectPage() {
+        $projects = new Collection;
+        foreach(auth()->user()->detail->kelas as $kelas) {
+            $projects = $projects->merge($kelas->project);
+        }
+        return view('guru.project.project', compact('projects'));
+    }
+
+    public function viewProject($id_project) {
+        $project = Project::where('id', $id_project)->firstOrFail();
+        if($project->kelas->guru != auth()->user()->detail) {
+            abort(404);
+        }
+        return view('guru.project.detail', compact('project'));
     }
     
 }
