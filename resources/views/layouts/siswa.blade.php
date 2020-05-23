f8a8e943d8aa01883eef06761cd7d9903d986ab4
Dexalt142 <faturjackson14@gmail.com>
2020-05-23 21:12:05 +0700

    Disable zoom on mobile device

--- resources/views/layouts/siswa.blade.php
+++ resources/views/layouts/siswa.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
         <title>@yield('page-title', 'PJBL Siswa')</title>
 
         <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
