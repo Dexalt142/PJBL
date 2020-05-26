168f4fbda6231c0c5e91b19aa31c8e719c5b9aa3
Dexalt142 <faturjackson14@gmail.com>
2020-05-26 23:30:28 +0700

    Added generate kelompok function

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Project;
+use App\Kelompok;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class ProjectController extends Controller {
 
@@ -38,6 +41,43 @@ class ProjectController extends Controller {
             abort(404);
         }
         return view('guru.project.detail', compact('project'));
+    }
+
+    public function generateKelompok(Request $request) {
+        $validated = $request->validate([
+            'id_project' => ['exists:project,id', 'required'],
+            'jumlah_siswa' => ['required', 'integer']
+        ]);
+
+        $proj = Project::find($validated['id_project']);
+        $siswaList = $proj->kelas->siswa->shuffle();
+        $max = $validated['jumlah_siswa'];
+
+        if($max > $siswaList->count() || $max < 1) {
+            return redirect()->back();
+        } else {
+            $n = 1;
+            $k = 1;
+            foreach ($siswaList as $siswa) {
+                $nama = "Kelompok $k";
+                $kelompok = Kelompok::where(['project_id' => $proj->id, 'nama_kelompok' => $nama])->first();
+                
+                if(!$kelompok) {
+                    $kelompok = Kelompok::create(['project_id' => $proj->id, 'nama_kelompok' => $nama]);
+                }
+                $ksid = DB::table('kelas_siswa')->where(['siswa_id' => $siswa->id, 'kelas_id' => $proj->kelas->id])->first('id');
+                
+                DB::table('kelompok_anggota')->insert(['kelompok_id' => $kelompok->id, 'siswa_id' => $ksid->id, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]);
+                    
+                if($n % $max == 0) {
+                    $k++;
+                }
+                $n++;
+            }
+
+        }
+
+        return redirect()->back();
     }
     
 }
