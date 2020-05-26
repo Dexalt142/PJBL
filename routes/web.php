06df083650cecf6051131c5a906e1503f059531b
Dexalt142 <faturjackson14@gmail.com>
2020-05-26 23:30:46 +0700

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -39,11 +39,12 @@ Route::middleware('auth')->group(function() {
 
 Route::middleware('guru', 'user-validated')->group(function() {
     Route::get('guru', 'GuruPageController@showDashboard')->name('guru-dashboard');
-
+    
     Route::prefix('guru')->group(function() {
         Route::prefix('kelas')->group(function() {
             Route::post('buat', 'KelasController@buatKelas');
             Route::get('{kelas}', 'GuruPageController@viewKelas')->name('guru-kelas-detail');
+            Route::post('{kelas}/undang', 'KelasController@undangSiswa');
             Route::post('edit/{kelas}', 'KelasController@editKelas');
             
             Route::post('gencode/{kelas}', 'KelasController@generateNewCode');
@@ -52,6 +53,7 @@ Route::middleware('guru', 'user-validated')->group(function() {
         Route::prefix('project')->group(function() {
             Route::get('/', 'ProjectController@showProjectPage')->name('guru-project');
             Route::get('{id_project}', 'ProjectController@viewProject')->name('guru-project-detail');
+            Route::post('{id_project}/gen-kelompok', 'ProjectController@generateKelompok');
             Route::post('buat', 'ProjectController@buatProject');
         });
 
@@ -65,3 +67,17 @@ Route::middleware('siswa', 'user-validated')->group(function() {
 });
 
 
+Route::get('/test', function() {
+    $users = App\User::all();
+    $i = 1;
+    $k = 1;
+    $max = 2;
+    foreach($users as $user) {
+        echo "Kel $k: $user->email<br>";
+        if($i % $max == 0) {
+            echo "==========<br>";
+            $k++;
+        }
+        $i++;
+    }
+});
