c00e7a93e8ac6fe814a4120ff5b1789ca10160ba
Dexalt142 <faturjackson14@gmail.com>
2020-05-29 23:16:24 +0700

    Added fase timeline design

--- resources/views/component/guru/card-fase.blade.php
+++ resources/views/component/guru/card-fase.blade.php
@@ -1,21 +1,31 @@
-<div class="col-12 mb-4">
-    <div class="card">
-        <div class="card-body">
-            <div class="row">
-                <div class="col-md-4 mb-4">
-                    <h4 class="m-0 f-w-700">
-                        {{ $fase->nama_fase }}
-                    </h4>
-                </div>
+<div class="fase">
+    <div class="number">
+        {{ $fase->fase_ke }}
+    </div>
+    <div class="col-12 mb-4">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-8 mb-3">
+                        <h4 class="m-0 f-w-700 mb-2">
+                            {{ $fase->nama_fase }}
+                        </h4>
+                        <p>
+                            {{ $fase->deskripsi }}
+                        </p>
+                    </div>
 
-                <div class="col-md-4 mb-4">
-                    <h5><ion-icon name="calendar-outline"></ion-icon> Deadline</h5>
-                    {{ \Carbon\Carbon::parse($fase->deadline)->format("d M Y H:i:s") }}
-                </div>
-
-                <div class="col-md-4 mb-4">
-                    <h5><ion-icon name="people-outline"></ion-icon> Kelompok selesai</h5>
-                    
+                    <div class="col-md-4 mb-4">
+                        <div class="f-w-700">
+                            <ion-icon name="calendar-outline"></ion-icon> Deadline: {{ \Carbon\Carbon::parse($fase->deadline)->format("d M Y H:i:s") }}
+                        </div>
+                        <div class="f-w-700">
+                            <ion-icon name="document-outline"></ion-icon> Tipe fase: {{ ucfirst($fase->fase_type) }}
+                        </div>
+                        <div class="f-w-700">
+                            <ion-icon name="checkbox-outline"></ion-icon> Kelompok selesai: 0
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
